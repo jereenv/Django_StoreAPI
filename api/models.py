6afbd3a3ab091c7cfd64b1c06a04fa5ae61b2bc7
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-27 19:53:19 -0700

    Added Item and Location model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.location_name
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add= True)
+    item_location = models.ForeignKey(Location, on_delete= models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.item_name
+
+
+
+
